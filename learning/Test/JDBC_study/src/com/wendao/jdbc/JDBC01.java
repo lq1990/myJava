f5cf2d7c4fc9d1bf02b07bdea1012d93c0abb727
lqLinux <chinalq90@gmail.com>
2019-06-10 14:09:47 +0200

    jdbc on linux

---
+++ learning/Test/JDBC_study/src/com/wendao/jdbc/JDBC01.java
@@ -0,0 +1,41 @@
+package com.wendao.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+
+public class JDBC01 {
+	public static void main(String[] args) {
+		Connection conn = null;
+		Statement stmt = null;
+		try {
+			// load Driver class
+			Class.forName("com.mysql.jdbc.Driver");
+			// connect
+			conn = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/db01?useUnicode=true&characterEncoding=UTF-8&useSSL=false", 
+					"lq", 
+					"123456");
+			
+			stmt = conn.createStatement();
+			stmt.executeUpdate("insert into tb_human (id,name) values (1001,'张三')");
+			
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			
+		}
+		
+		System.out.println("over");
+	}
+}
+
+
+
