021c0de2fd2c9dbbc693a9cb8ec6653ffc75e184
Qiang <chinalq90@gmail.com>
2020-01-05 21:06:33 +0800

    SpringBoot over, Spring Cloud... 微服务全家桶

---
+++ springboot-08-actuator/src/test/java/com/mycp/actuator/Springboot08ActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycp.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot08ActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
